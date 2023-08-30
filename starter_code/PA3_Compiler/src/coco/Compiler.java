288372020209fd21244c292d892fa04dfeac3499
Khanh Nguyen <khanhtn@tamu.edu>
2023-08-30 15:02:27 -0500

    starter code PA3

---
+++ starter_code/PA3_Compiler/src/coco/Compiler.java
@@ -0,0 +1,210 @@
+package coco;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class Compiler {
+
+    // Error Reporting ============================================================
+    private StringBuilder errorBuffer = new StringBuilder();
+
+    private String reportSyntaxError (NonTerminal nt) {
+        String message = "SyntaxError(" + lineNumber() + "," + charPosition() + ")[Expected a token from " + nt.name() + " but got " + currentToken.kind + ".]";
+        errorBuffer.append(message + "\n");
+        return message;
+    }
+
+    private String reportSyntaxError (Token.Kind kind) {
+        String message = "SyntaxError(" + lineNumber() + "," + charPosition() + ")[Expected " + kind + " but got " + currentToken.kind + ".]";
+        errorBuffer.append(message + "\n");
+        return message;
+    }
+
+    public String errorReport () {
+        return errorBuffer.toString();
+    }
+
+    public boolean hasError () {
+        return errorBuffer.length() != 0;
+    }
+
+    private class QuitParseException extends RuntimeException {
+        private static final long serialVersionUID = 1L;
+        public QuitParseException (String errorMessage) {
+            super(errorMessage);
+        }
+    }
+
+    private int lineNumber () {
+        return currentToken.lineNumber();
+    }
+
+    private int charPosition () {
+        return currentToken.charPosition();
+    }
+
+// Compiler ===================================================================
+    private Scanner scanner;
+    private Token currentToken;
+    
+    private int numDataRegisters; // available registers are [1..numDataRegisters]
+    private List<Integer> instructions;
+
+    // Need to map from IDENT to memory offset
+
+    public Compiler (Scanner scanner, int numRegs) {
+        this.scanner = scanner;
+        currentToken = this.scanner.next();
+        numDataRegisters = numRegs;
+        instructions = new ArrayList<>();
+    }
+
+    public int[] compile () {
+        try {
+            computation();
+            return instructions.stream().mapToInt(Integer::intValue).toArray();
+        }
+        catch (QuitParseException q) {
+            errorBuffer.append("SyntaxError(" + lineNumber() + "," + charPosition() + ")");
+            errorBuffer.append("[Could not complete parsing.]");
+            return new ArrayList<Integer>().stream().mapToInt(Integer::intValue).toArray();
+        }
+    }
+
+// Helper Methods =============================================================
+    private boolean have (Token.Kind kind) {
+        return currentToken.is(kind);
+    }
+
+    private boolean have (NonTerminal nt) {
+        return nt.firstSet().contains(currentToken.kind);
+    }
+
+    private boolean accept (Token.Kind kind) {
+        if (have(kind)) {
+            try {
+                currentToken = scanner.next();
+            }
+            catch (NoSuchElementException e) {
+                if (!kind.equals(Token.Kind.EOF)) {
+                    String errorMessage = reportSyntaxError(kind);
+                    throw new QuitParseException(errorMessage);
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private boolean accept (NonTerminal nt) {
+        if (have(nt)) {
+            currentToken = scanner.next();
+            return true;
+        }
+        return false;
+    }
+
+    private boolean expect (Token.Kind kind) {
+        if (accept(kind)) {
+            return true;
+        }
+        String errorMessage = reportSyntaxError(kind);
+        throw new QuitParseException(errorMessage);
+    }
+
+    private boolean expect (NonTerminal nt) {
+        if (accept(nt)) {
+            return true;
+        }
+        String errorMessage = reportSyntaxError(nt);
+        throw new QuitParseException(errorMessage);
+    }
+
+    private Token expectRetrieve (Token.Kind kind) {
+        Token tok = currentToken;
+        if (accept(kind)) {
+            return tok;
+        }
+        String errorMessage = reportSyntaxError(kind);
+        throw new QuitParseException(errorMessage);
+    }
+
+    private Token expectRetrieve (NonTerminal nt) {
+        Token tok = currentToken;
+        if (accept(nt)) {
+            return tok;
+        }
+        String errorMessage = reportSyntaxError(nt);
+        throw new QuitParseException(errorMessage);
+    }
+
+    // OPTIONAL: may be useful to have help functions for the following:
+    //              - branch forward
+    //              - fix branch instructions
+    //              - load/store
+    //              - computing arithmetic
+    //              - free/alloc reg
+
+// Pre-defined Functions ======================================================
+    private void readInt (int rDest) {
+        // TODO: implement with DLX.RDI
+    }
+
+    private void readFloat (int rDest) {
+        // TODO: implement with DLX.RDF
+    }
+
+    private void readBool (int rDest) {
+        // TODO: implement with DLX.RDB
+    }
+
+    private void printInt (int rSrc) {
+        // TODO: implement with DLX.WRI
+    }
+
+    private void printFloat (int rSrc) {
+        // TODO: implement with DLX.WRF
+    }
+
+    private void printBool (int rSrc) {
+        // TODO: implement with DLX.WRB
+    }
+
+    private void println () {
+        // TODO: implement with DLX.WRL
+    }
+
+// Grammar Rules ==============================================================
+
+    // function for matching rule that only expects nonterminal's FIRST set
+    private Token matchNonTerminal (NonTerminal nt) {
+        return expectRetrieve(nt);
+    }
+
+    // TODO: copy operators and type grammar rules from Interpreter
+
+    // literal = integerLit | floatLit
+    private Token literal () {
+        return matchNonTerminal(NonTerminal.LITERAL);
+    }
+
+    // designator = ident { "[" relExpr "]" }
+    private void designator () {
+        int lineNum = lineNumber();
+        int charPos = charPosition();
+
+        Token ident = expectRetrieve(Token.Kind.IDENT);
+
+        // TODO: load value from memory into register
+        // 
+    }
+
+    // TODO: copy remaining grammar rules from Interpreter and make edits to compile into DLX
+
+    // computation	= "main" {varDecl} {funcDecl} "{" statSeq "}" "."
+    private void computation () {
+        throw new QuitParseException("implement all grammar rules to generate DLX");
+    }
+}
